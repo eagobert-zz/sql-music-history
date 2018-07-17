379245867799be65c1b798d6a0da5e7d8b598905
eagobert <eagobert@hotmail.com>
2018-07-17 13:22:07 -0500

    Initial commit. still working

---
+++ queries.sql
@@ -0,0 +1,34 @@
+-- Q1: Query all of the entries in the Genre table
+SELECT *
+FROM Genre
+
+-- Q2: Using the INSERT statement, add one of your favorite artists to the Artist table.
+INSERT INTO Artist
+VALUES
+(null, "Beyonce", 1996);
+
+-- Q3: Using the INSERT statement, add one, or more, albums by your artist to the Album table.
+INSERT INTO Album
+VALUES
+(null, "Lemonade", 2016, 400, "Roc Nation", 28, 6),
+(null, "I am Sasha Fierce", 2010, 400, "Roc Nation", 28, 6)
+
+-- Q4: Using the INSERT statement, add some songs that are on that album to the Song table.
+INSERT INTO Song
+VALUES
+(null, "Sorry", 120, "09/16/2016", 6, 28, 23),
+(null, "Hold Up", 400, "09/16/2016", 6, 28, 23)
+
+-- Q5: Write a SELECT query that provides the song titles, album title, and artist name for all of the data you just entered in. Use the LEFT JOIN keyword sequence to connect the tables, and the WHERE keyword to filter the results to the album and artist you added.
+
+-- Q6: Write a SELECT statement to display how many songs exist for each album. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+
+-- Q7: Write a SELECT statement to display how many songs exist for each artist. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+
+-- Q8: Write a SELECT statement to display how many songs exist for each genre. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+
+-- Q9: Using MAX() function, write a select statement to find the album with the longest duration. The result should display the album title and the duration.
+
+-- Q10: Using MAX() function, write a select statement to find the song with the longest duration. The result should display the song title and the duration.
+
+-- Q11: Modify the previous query to also display the title of the album.
